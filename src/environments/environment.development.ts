d3068cb7df4403ab1aea6de99beb720eab746d68
elias <eliasvinharski19@gmail.com>
2023-04-19 16:13:41 -0300

    AULA-ANGULAR-KANBAN
    Usamos o codelab para reproduzir este site.

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanbanfire-6b50d',
+    appId: '1:640595783410:web:b38c40045b1336348d6a6b',
+    databaseURL: 'https://kanbanfire-6b50d-default-rtdb.firebaseio.com',
+    storageBucket: 'kanbanfire-6b50d.appspot.com',
+    apiKey: 'AIzaSyDMWsdqjzi8Dcz2cypONd7U2tv5Oo4K7rU',
+    authDomain: 'kanbanfire-6b50d.firebaseapp.com',
+    messagingSenderId: '640595783410',
+    measurementId: 'G-7S5WJEL7Z5',
+  },};
